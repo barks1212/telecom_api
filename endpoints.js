584910d843265496a1ca1d798bb43ac661bceead
barks1212 <willbarkerwork@gmail.com>
2019-08-11 21:40:07 +0100

    final endpoint for activate number

--- endpoints.js
+++ endpoints.js
@@ -3,8 +3,14 @@ const teleComProvider = require('./services')(data);
 
 const initialiseEndpoints = (app) => {
     app.get('/numbers', (req, res) => res.send(teleComProvider.getAll()));
-    app.get('/numbers/:id', (req, res) => res.send(teleComProvider.getNumberById(req.params.id)));
-    app.put('/numbers/activate/:id', (req, res) => res.send(teleComProvider.activateNumber(req.params.id)));
+    app.get('/numbers/:id(\\d+)/', (req, res) => res.send(teleComProvider.getNumberById(req.params.id)));
+    app.put('/numbers/:id(\\d+)/activate/:number', (req, res) => res.send(teleComProvider.activateNumber(req.params.id)));
+    app.use((err, req, res, next) => {
+        if(err.name === "CustomerNotFound") {
+            res.status(404);
+        }
+        res.json(err);
+    });
 }
 
 module.exports = initialiseEndpoints;
