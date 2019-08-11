c8cc049c3ed0fa162951da549fb5a1c149f90dbb
barks1212 <willbarkerwork@gmail.com>
2019-08-11 15:22:38 +0100

    hook up service with server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const teleComProvider = require('./services');
+const data = require('./data/customerData.json')
 
 const app = express();
 
 const port = 3000;
 
-app.get('/all',provider.getAll);
+app.get('/all', (req, res) => res.send(teleComProvider(data).getAll()));
 
-app.listen(port);
+app.listen(port, () => console.log(`Listening on port ${port}!`));
--- services/index.test.js
+++ services/index.test.js
@@ -2,29 +2,6 @@ const teleComProvider = require('./');
 
 describe('get all phone numbers', () => {
 
-    const customers = [{
-        id: '1',
-        phoneNumbers: [{
-            number: 1234,
-            activated: false
-        },
-        {
-            number: 5678,
-            activated: false
-        }],
-    },
-    {
-        id: '2',
-        phoneNumbers: [{
-            number: 91011,
-            activated: false
-        },
-        {
-            number: 121314,
-            activated: false
-        }],
-    }];
-
     it('should return an empty array when customerData is empty', () => {
         const provider = teleComProvider([]);
         const result = provider.getAll();
@@ -38,6 +15,22 @@ describe('get all phone numbers', () => {
     });
 
     it ('should return all phone numbers', () => {
+        const customers = [{
+            phoneNumbers: [{
+                number: 1234,
+            },
+            {
+                number: 5678,
+            }],
+        },
+        {
+            phoneNumbers: [{
+                number: 91011
+            },
+            {
+                number: 121314
+            }],
+        }];
         const provider = teleComProvider(customers);
         const result = provider.getAll();
         expect(result).toEqual([1234, 5678, 91011, 121314]);
