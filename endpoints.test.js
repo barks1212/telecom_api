f42317410ea8c943f7ef88ebdebdbb381ad79e9e
barks1212 <willbarkerwork@gmail.com>
2019-08-11 21:58:46 +0100

    basic error handling tests for activate endpoint

--- endpoints.test.js
+++ endpoints.test.js
@@ -32,3 +32,43 @@ describe('getNumberById', () => {
         })
     })
 });
+
+describe('activateNumber', () => {
+
+    beforeAll(() => {
+        endpoints(app)
+        request = require('supertest')(app);
+    })
+    
+    it('should 404 when passed incorrect path', done => {
+        request.get('/numbers/not-a-number/activate/077077077').expect(404, done);
+    })
+
+    describe('Given customer doesnt exist', () => {
+
+        let activeNumberById;
+
+        beforeAll(() => {
+            activeNumberById = request.put('/numbers/4/activate/077077077');
+        })
+
+        it('should return 404', () => {
+            activeNumberById.expect(404);
+        })
+        it('return correct error message', done => {
+            activeNumberById.then(res => {
+                expect(res.body).toEqual({name: 'CustomerNotFound'})
+                done();
+            });
+        })
+    })
+
+    describe('Given customer exist', () => {
+
+        let activeNumberById;
+
+        it('should return 404 when phone number doesnt exist', () => {
+            request.put('/numbers/1/activate/077077077').expect(404);
+        })
+    })
+});
