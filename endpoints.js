f8a4980df32be3d5d67c672071c0b46b1190b712
barks1212 <willbarkerwork@gmail.com>
2019-08-12 18:40:11 +0100

    patch endpoint with error handling

--- endpoints.js
+++ endpoints.js
@@ -1,12 +1,16 @@
 const data = require('./data/customerData.json');
+
 const teleComProvider = require('./services')(data);
 
 const initialiseEndpoints = (app) => {
     app.get('/numbers', (req, res) => res.send(teleComProvider.getAll()));
     app.get('/numbers/:id(\\d+)/', (req, res) => res.send(teleComProvider.getNumberById(req.params.id)));
-    app.put('/numbers/:id(\\d+)/activate/:number', (req, res) => res.send(teleComProvider.activateNumber(req.params.id)));
+    app.patch('/numbers/:id(\\d+)/activate/:number', (req, res) => res.send(teleComProvider.activateNumber(req.params.id)));
     app.use((err, req, res, next) => {
         if(err.name === "CustomerNotFound") {
+            res.status(404);
+        }
+        if (err.name ===  "NumberNotFound") {
             res.status(404);
         }
         res.json(err);
